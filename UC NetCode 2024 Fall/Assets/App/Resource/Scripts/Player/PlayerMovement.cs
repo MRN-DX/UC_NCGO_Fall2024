665779377203d33f891bd8c74ef5ecb93256cec4
MRN-DX <matthew.necamp@cchmc.org>
2024-09-09 22:56:11 -0400

    Module 3
    -Added ClientNetworkTransofrm script
    -Added simple player count Networked variable, synced and tested.
    -Added basic ownership to player movement, still need to polish actual movement
    -Start button for host/server

--- UC NetCode 2024 Fall/Assets/App/Resource/Scripts/Player/PlayerMovement.cs
+++ UC NetCode 2024 Fall/Assets/App/Resource/Scripts/Player/PlayerMovement.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 namespace App.Resource.Scripts.Player
 {
 
-    public class PlayerMovement : MonoBehaviour
+    public class PlayerMovement : NetworkBehaviour
     {
         private void Update()
         {
-           
+
+            if (!IsOwner) return;
+            
                 Vector3 moveDirection = new Vector3(0, 0, 0);
                 if (Input.GetKey(KeyCode.W)) moveDirection.z = +1f;
                 if (Input.GetKey(KeyCode.S)) moveDirection.z = -1f;
