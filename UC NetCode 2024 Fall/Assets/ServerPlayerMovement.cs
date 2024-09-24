ab351366b76c89a457129722914433a31948ed6e
MRN-DX <matthew.necamp@cchmc.org>
2024-09-23 22:08:24 -0400

    Server auth prefab
    -added server auth
    -with new input system

--- UC NetCode 2024 Fall/Assets/ServerPlayerMovement.cs
+++ UC NetCode 2024 Fall/Assets/ServerPlayerMovement.cs
@@ -1,18 +1,64 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.Netcode;
 
-public class ServerPlayerMovement : MonoBehaviour
+[RequireComponent(typeof(CharacterController))]
+public class ServerPlayerMovement : NetworkBehaviour
 {
+
+    [SerializeField] private float _pSpeed;
+    [SerializeField] private Transform _pTransform;
+
+    public CharacterController _CC;
+    private MyPlayerInputActions _playerInput;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        _playerInput = new();
+        _playerInput.Enable();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+        if(!IsOwner) return;
+
+        // Read our player input from our new input system
+        Vector2 moveInput = _playerInput.Player.Movement.ReadValue<Vector2>();
+        
+        
+        
+        // Determine if we are a server or a player
+
+        if (IsServer)
+        {
+            // Move if server
+            Move(moveInput);
+        }
+        else if (IsClient && !IsHost)
+        {
+            // Send a move request rpc to move the playerr.
+            MoveServerRPC(moveInput);
+        }
+        
     }
+    
+    private void Move(Vector2 _input)
+    {
+        Vector3 _moveDirection = _input.x * _pTransform.right + _input.y * _pTransform.forward;
+
+        _CC.Move(_moveDirection * _pSpeed * Time.deltaTime);
+
+    }
+
+    [Rpc(SendTo.Server)]
+   private void MoveServerRPC(Vector2 _input)
+   {
+       Move(_input);
+   }
+    
+    
 }
